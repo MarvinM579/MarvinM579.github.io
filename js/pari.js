89e832ee540b2f060f84941aefedc2a6342715cd
Marvin <marvinmartinez579@gmail.com>
2018-05-05 17:29:07 -0500

    idk

--- js/pari.js
+++ js/pari.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
     $(".yes").click(function() {
         $(".hideyes").removeClass("hideyes");
+        $(".yes").addClass("hideyes");
+        $(".no").addClass("hideno");
     })
     $(".no").click(function() {
         $(".hideno").removeClass("hideno");
+        $(".yes").addClass("hideyes");
+        $(".no").addClass("hideno");
     })
 });
